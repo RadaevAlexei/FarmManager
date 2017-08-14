17bda2800c2b844ac08b17947933c65f5387a7d6
RadaevAlexei <alexeiradaev@gmail.com>
2017-08-14 23:13:56 +0400

    рефакторинг. часть 3

--- fermer/backend/views/suspension/index.php
+++ fermer/backend/views/suspension/index.php
@@ -1,55 +1,25 @@
 <?php
 
-use yii\helpers\Html;
-use yii\widgets\LinkPager;
-use yii\helpers\ArrayHelper;
-use \yii\helpers\Url;
+use \yii\grid\GridView;
+use \common\models\search\SuspensionSearch;
 
-?>
+$this->title = 'Список перевесок';
+$this->params['breadcrumbs'][] = $this->title;
 
-<h2><?=\Yii::t('app/back', 'SUSPENSIONS')?></h2>
-<table class="table f-table-list table-striped table-hover table-condensed">
+/** @var $dataProvider \yii\data\ActiveDataProvider */
+/** @var $searchModel SuspensionSearch */
 
-    <thead>
-        <tr>
-            <th>№</th>
-            <th>Дата взвешивания</th>
-            <th>Теленок</th>
-            <th>Вес</th>
-            <th></th>
-        </tr>
-    </thead>
-
-    <tbody>
-        <?php if (!empty($suspensions)) : ?>
-            <?php foreach ($suspensions as $index => $suspension): ?>
-                <tr data-id="<?=$suspension["id"]?>" style="cursor: pointer">
-                    <td><?=($index + 1)?></td>
-                    <td><?=ArrayHelper::getValue($suspension, "date", "")?></td>
-                    <td><?=ArrayHelper::getValue($suspension, "calf", "")?></td>
-                    <td><?=ArrayHelper::getValue($suspension, "weight", "")?></td>
-                    <td align="center">
-                        <div class="btn-group">
-                            <button type="button" class="btn btn-warning btn-sm  dropdown-toggle" data-toggle="dropdown">Действие <span class="caret"></span></button>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="<?=Url::toRoute(['/suspension/edit/' . $suspension['id'] . '/'])?>">Редактировать</a></li>
-                                <li><a href="<?=Url::toRoute(['/suspension/delete/' . $suspension['id'] . '/'])?>">Удалить</a></li>
-                            </ul>
-                        </div>
-                    </td>
-                </tr>
-            <?php endforeach; ?>
-        <?php else: ?>
-            <tr>
-                <td align="center" colspan="5"><?=\Yii::t('app/back', 'SUSPENSIONS_NOT_FOUND')?></td>
-            </tr>
-        <?php endif; ?>
-
-    </tbody>
-</table>
-
-<div class="pull-right">
-    <button type="button" data-url="<?=Url::toRoute(['/suspension/new/'])?>" class="btn btn-info addItem">Добавить</button>
-</div>
-
-<?= LinkPager::widget(['pagination' => $pagination]) ?>
+echo GridView::widget([
+    "dataProvider" => $dataProvider,
+    "filterModel"  => $searchModel,
+    'columns'      => [
+        ['class' => 'yii\grid\SerialColumn'],
+        'calf',
+        'weight',
+        'date',
+        [
+            'class'  => 'yii\grid\ActionColumn',
+            'header' => 'Действия'
+        ],
+    ]
+]);
