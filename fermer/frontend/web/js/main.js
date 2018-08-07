6db492e13427e6f46f53c674ac6bc48be5de1067
RadaevAlexei <alexeiradaev@gmail.com>
2018-08-07 16:09:19 +0400

    вынесение из папки FarmManager

--- fermer/frontend/web/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $(document).on("click", "button#addFunction, button#addEmployee", function (e) {
-        alert(3454567567);
-        //url = $(this).data("url");
-        //document.location.href = url;
-    });
-
-});
