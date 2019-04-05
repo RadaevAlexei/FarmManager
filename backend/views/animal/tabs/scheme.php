8dc3929a00525afaff67b6760956186b57f12b30
RadaevAlexei <alexeiradaev@gmail.com>
2019-04-05 19:14:12 +0400

    изменил вывод постановки на схему

--- backend/views/animal/tabs/scheme.php
+++ backend/views/animal/tabs/scheme.php
@@ -24,49 +24,52 @@ $healthModel = new HealthForm();
 
 ?>
 
-    <div class="box box-success">
-        <div class="box-header with-border">
-            <h3 class="box-title">Диагноз</h3>
-        </div>
-        <?php $form = ActiveForm::begin([
-            'action' => Url::toRoute(['update-health']),
-            'method' => 'post',
-            'id'     => 'tttwrter',
-            'class'  => 'form-horizontal',
-        ]); ?>
-        <div class="box-body">
-            <div class="form-group">
-                <?= $form->field($healthModel, 'animal_id')->hiddenInput(['value' => $animal->id])->label(false) ?>
-                <?= $form->field($healthModel, 'health_status')->dropDownList(
-                    Animal::getHealthStatusList(),
-                    [
-                        'id'     => 'health_status_list',
-                        'prompt' => 'Выберите статус здоровья',
-                        'class'  => 'form-control',
-                        'value'  => $animal->health_status,
-                    ]
-                ) ?>
-            </div>
-            <div id="diagnosis_list" class="form-group <?= ($animal->health_status == 0 ? 'hidden' : '') ?>">
-                <?= $form->field($healthModel, 'diagnosis')->dropDownList(
-                    ArrayHelper::map(Diagnosis::getAllList(), "id", "name"),
-                    [
-                        'prompt' => 'Выберите диагноз',
-                        'class'  => [
-                            'form-control'
-                        ],
-                        'value'  => $animal->diagnosis,
-                    ]
-                ) ?>
-            </div>
-        </div>
-        <div class="box-footer">
-            <button type="submit" class="btn btn-primary">Поставить диагноз</button>
-        </div>
-        <?php ActiveForm::end() ?>
+<div class="box box-success">
+    <div class="box-header with-border" style="background-color: #0ead0e78">
+        <h3 class="box-title">Диагноз</h3>
     </div>
+    <?php $form = ActiveForm::begin([
+        'action' => Url::toRoute(['update-health']),
+        'method' => 'post',
+        'id'     => 'update-health-form',
+        'class'  => 'form-horizontal',
+    ]); ?>
+    <div class="box-body">
+        <div class="form-group">
+            <?= $form->field($healthModel, 'animal_id')->hiddenInput(['value' => $animal->id])->label(false) ?>
+            <?= $form->field($healthModel, 'health_status')->dropDownList(
+                Animal::getHealthStatusList(),
+                [
+                    'id'     => 'health_status_list',
+                    'prompt' => 'Выберите статус здоровья',
+                    'class'  => 'form-control',
+                    'value'  => $animal->health_status,
+                ]
+            ) ?>
+        </div>
+        <div id="diagnosis_list" class="form-group <?= ($animal->health_status == 0 ? 'hidden' : '') ?>">
+            <?= $form->field($healthModel, 'diagnosis')->dropDownList(
+                ArrayHelper::map(Diagnosis::getAllList(), "id", "name"),
+                [
+                    'prompt' => 'Выберите диагноз',
+                    'class'  => [
+                        'form-control'
+                    ],
+                    'value'  => $animal->diagnosis,
+                ]
+            ) ?>
+        </div>
+    </div>
+    <div class="box-footer">
+        <?= Html::submitButton('Поставить диагноз', [
+            'class' => 'btn btn-primary',
+            'data'  => ['confirm' => 'Вы действительно хотите поставить такой диагноз?']
+        ]) ?>
+    </div>
+    <?php ActiveForm::end() ?>
+</div>
 
-<?php if ($animalOnScheme) {
+<?php if ($animalOnScheme) :
     echo Html::tag('span', 'Животное находится на схеме ' .
         Html::tag('span', ArrayHelper::getValue($animalOnScheme, 'scheme.name'), [
             'class' => 'label label-danger'
@@ -89,48 +92,57 @@ $healthModel = new HealthForm();
         'actionsToday' => $actionsToday
     ]);
 
-} else {
-    $form = ActiveForm::begin([
-        'action' => Url::toRoute(['animal/appropriation-scheme']),
-        'id'     => 'animal-form',
-        'layout' => 'horizontal',
-        'method' => 'post',
-        'class'  => 'form-horizontal'
-    ]); ?>
+else: ?>
 
-    <div class="form-group">
-        <div class="col-sm-10">
-            <?= $form->field($appropriationScheme, 'animal_id')->textInput(['class' => 'hidden'])->label(false); ?>
-            <?= $form->field($appropriationScheme, 'status')->textInput(['class' => 'hidden'])->label(false); ?>
+    <div class="box box-success">
+        <div class="box-header with-border" style="background-color: #0ead0e78">
+            <h3 class="box-title">Поставить на схему лечения</h3>
+        </div>
 
-            <?= $form->field($appropriationScheme, 'started_at')->widget(DatePicker::class, [
-                'language'   => 'ru',
-                'dateFormat' => 'yyyy-MM-dd',
-                'class'      => 'form-control',
-                //                'clientOptions' => [
-                //                    'autocomplete' => 'off'
-                //                ]
+        <?php $form = ActiveForm::begin([
+            'action' => Url::toRoute(['appropriation-scheme']),
+            'id'     => 'animal-form',
+            'method' => 'post',
+            'class'  => 'form-horizontal'
+        ]); ?>
+        <div class="box-body">
+
+            <div class="form-group">
+                <div class="col-sm-10">
+                    <?= $form->field($appropriationScheme, 'animal_id')->hiddenInput()->label(false); ?>
+                    <?= $form->field($appropriationScheme, 'status')->hiddenInput()->label(false); ?>
+
+                    <?= $form->field($appropriationScheme, 'started_at')->widget(DatePicker::class, [
+                        'language'   => 'ru',
+                        'dateFormat' => 'yyyy-MM-dd',
+                        'options'    => [
+                            'class'        => 'form-control',
+                            'autocomplete' => 'off'
+                        ]
+                    ]) ?>
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-sm-10">
+                    <?= $form->field($appropriationScheme, 'scheme_id')->dropDownList(
+                        $schemeList,
+                        [
+                            'class'  => 'form-control',
+                            'prompt' => 'Выберите схему'
+                        ]
+                    ) ?>
+                </div>
+            </div>
+        </div>
+
+        <div class="box-footer">
+            <?= Html::submitButton('Поставить на схему', [
+                'class' => 'btn btn-primary',
+                'data'  => ['confirm' => 'Вы действительно хотите поставить на эту схему?']
             ]) ?>
         </div>
+        <?php ActiveForm::end(); ?>
     </div>
 
-    <div class="form-group">
-        <div class="col-sm-10">
-            <?= $form->field($appropriationScheme, 'scheme_id')->dropDownList(
-                $schemeList,
-                [
-                    'class'  => 'form-control',
-                    'prompt' => 'Выберите схему'
-                ]
-            ) ?>
-        </div>
-    </div>
-
-    <div class="form-group">
-        <div class="col-sm-offset-2 col-sm-10">
-            <button type="submit" class="btn btn-danger">Поставить на схему</button>
-        </div>
-    </div>
-    <?php ActiveForm::end();
-
-} ?>
+<?php endif; ?>
