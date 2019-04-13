b124fcce609b00e6c9a0bf6f8ca18586a65a1255
RadaevAlexei <alexeiradaev@gmail.com>
2019-04-13 18:35:43 +0400

    список складов обернул в блок

--- backend/modules/pharmacy/views/stock/index.php
+++ backend/modules/pharmacy/views/stock/index.php
@@ -16,46 +16,52 @@ $this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-    <div class="form-group">
-        <?= Html::a(
-            'Добавить склад',
-            Url::toRoute(['stock/new']),
-            ['class' => 'btn btn-primary']
-        ) ?>
-    </div>
+<div class="form-group">
+    <?= Html::a(
+        'Добавить склад',
+        Url::toRoute(['stock/new']),
+        ['class' => 'btn btn-primary']
+    ) ?>
+</div>
 
-<?php echo GridView::widget([
-    "dataProvider" => $dataProvider,
-    "filterModel" => $searchModel,
-    'tableOptions' => [
-        'class' => 'table table-striped',
-    ],
-    'columns' => [
-        ['class' => 'yii\grid\SerialColumn'],
-        'name',
-        [
-            'class' => 'yii\grid\ActionColumn',
-            'header' => 'Действия',
-            'template' => '<div class="btn-group">{update} {delete} </div>',
-            'buttons' => [
-                'update' => function ($url, $model) {
-                    return Html::a(
-                        '<span class="glyphicon glyphicon-edit"></span>',
-                        Url::toRoute(['stock/edit', 'id' => $model->id]),
-                        ['class' => 'btn btn-warning']
-                    );
-                },
-                'delete' => function ($url, $model) {
-                    return Html::a(
-                        '<span class="glyphicon glyphicon-trash"></span>',
-                        Url::toRoute(['stock/delete', 'id' => $model->id]),
-                        [
-                            'class' => 'btn btn-danger',
-                            'data' => ['confirm' => 'Вы действительно хотите удалить склад?']
-                        ]
-                    );
-                },
+<div class="box box-info">
+    <div class="box-body">
+
+        <?php echo GridView::widget([
+            "dataProvider" => $dataProvider,
+            "filterModel"  => $searchModel,
+            'tableOptions' => [
+                'class' => 'table table-striped',
             ],
-        ],
-    ]
-]);
+            'columns'      => [
+                ['class' => 'yii\grid\SerialColumn'],
+                'name',
+                [
+                    'class'    => 'yii\grid\ActionColumn',
+                    'header'   => 'Действия',
+                    'template' => '<div class="btn-group">{update} {delete} </div>',
+                    'buttons'  => [
+                        'update' => function ($url, $model) {
+                            return Html::a(
+                                '<span class="glyphicon glyphicon-edit"></span>',
+                                Url::toRoute(['stock/edit', 'id' => $model->id]),
+                                ['class' => 'btn btn-warning']
+                            );
+                        },
+                        'delete' => function ($url, $model) {
+                            return Html::a(
+                                '<span class="glyphicon glyphicon-trash"></span>',
+                                Url::toRoute(['stock/delete', 'id' => $model->id]),
+                                [
+                                    'class' => 'btn btn-danger',
+                                    'data'  => ['confirm' => 'Вы действительно хотите удалить склад?']
+                                ]
+                            );
+                        },
+                    ],
+                ],
+            ]
+        ]); ?>
+
+    </div>
+</div>
