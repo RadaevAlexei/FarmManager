317a394237dcf4257dd20c1214c644bc03182145
RadaevAlexei <alexeiradaev@gmail.com>
2019-05-30 09:48:12 +0400

    в класс Preparation добавил новые поля

--- backend/modules/pharmacy/models/Preparation.php
+++ backend/modules/pharmacy/models/Preparation.php
@@ -10,6 +10,10 @@ use yii\db\ActiveRecord;
  * @package backend\modules\pharmacy\models
  *
  * @property string $name
+ * @property integer $category
+ * @property integer $period_milk
+ * @property integer $period_meat
+ * @property integer $danger_class
  */
 class Preparation extends ActiveRecord
 {
@@ -32,7 +36,11 @@ class Preparation extends ActiveRecord
     public function attributeLabels()
     {
         return [
-            'name' => 'Название препарата'
+            'name'         => 'Название препарата',
+            'category'     => 'Категория',
+            'period_milk'  => 'Период выведения молока',
+            'period_meat'  => 'Период выведения мяса',
+            'danger_class' => 'Класс опасности',
         ];
     }
 
@@ -45,7 +53,63 @@ class Preparation extends ActiveRecord
             [['name'], 'unique'],
             [['name'], 'trim'],
             [['name'], 'required'],
-            [['name'], 'string', 'max' => 255]
+            [['name'], 'string', 'max' => 255],
+            [['category', 'danger_class', 'period_milk', 'period_meat'], 'integer'],
         ];
+    }
+
+    /**
+     * Получение списка препаратов
+     * @return array|ActiveRecord[]
+     */
+    public static function getAllList()
+    {
+        return self::find()->all();
+    }
+
+    /**
+     * @return array
+     */
+    public static function getCategoryList()
+    {
+        return [
+            1 => 'Антибактериальные средства',
+            2 => 'НПВС',
+            3 => 'Витамины',
+            4 => 'Седативные средства',
+            5 => 'Дезинфицирующие средства',
+            6 => 'Инсептициды',
+            7 => 'Противопаразитарные',
+            8 => 'Вакцины',
+            9 => 'Прочее',
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public static function getDangerClass()
+    {
+        return [
+            1 => 1,
+            2 => 2,
+            3 => 3,
+        ];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCategoryName()
+    {
+        return self::getCategoryList()[$this->category];
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDangerClassName()
+    {
+        return self::getDangerClass()[$this->danger_class];
     }
 }
