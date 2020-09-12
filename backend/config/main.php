1fbf4772d69fefa5647d803cd2dfbd1c43fab85b
RadaevAlexei <alexeiradaev@gmail.com>
2020-09-12 12:46:14 +0400

    AdminLTE3. пример рендера таблицы

--- backend/config/main.php
+++ backend/config/main.php
@@ -1,4 +1,10 @@
 <?php
+
+use \yii\widgets\LinkPager;
+use \yii\bootstrap4\LinkPager as LinkPagerBootstrap4;
+
+;
+
 $params = array_merge(
     require(__DIR__ . '/../../common/config/params.php'),
     require(__DIR__ . '/../../common/config/params-local.php'),
@@ -7,31 +13,40 @@ $params = array_merge(
 );
 
 return [
-    'id'                  => 'app-backend',
-    'name'                => "СТАДО ООО \"АГРО-НЕПТУН\"",
-    'basePath'            => dirname(__DIR__),
+    'id' => 'app-backend',
+    'name' => "СТАДО ООО \"АГРО-НЕПТУН\"",
+    'basePath' => dirname(__DIR__),
     'controllerNamespace' => 'backend\controllers',
-    'language'            => 'ru-RU',
-    'sourceLanguage'      => 'ru-RU',
-    'bootstrap'           => ['log'],
-    'components'          => [
-        'request'      => [
+    'language' => 'ru-RU',
+    'sourceLanguage' => 'ru-RU',
+    'bootstrap' => ['log'],
+    'container' => [
+        'definitions' => [
+            LinkPager::class => LinkPagerBootstrap4::class,
+            'yii\bootstrap4\LinkPager' => [
+                'firstPageLabel' => 'Первая',
+                'lastPageLabel' => 'Последняя'
+            ]
+        ],
+    ],
+    'components' => [
+        'request' => [
             'csrfParam' => '_csrf-backend',
         ],
-        'user'         => [
-            'identityClass'   => 'common\models\User',
+        'user' => [
+            'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie'  => ['name' => '_identity-backend', 'httpOnly' => true],
+            'identityCookie' => ['name' => '_identity-backend', 'httpOnly' => true],
         ],
-        'session'      => [
+        'session' => [
             // this is the name of the session cookie used for login on the backend
             'name' => 'advanced-backend',
         ],
-        'log'          => [
+        'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets'    => [
+            'targets' => [
                 [
-                    'class'  => 'yii\log\FileTarget',
+                    'class' => 'yii\log\FileTarget',
                     'levels' => ['error', 'warning'],
                 ],
             ],
@@ -39,59 +54,59 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        'urlManager'   => [
-            'class'               => '\yii\web\UrlManager',
-            'enablePrettyUrl'     => true,
-            'showScriptName'      => false,
+        'urlManager' => [
+            'class' => '\yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
             'enableStrictParsing' => true,
-            'suffix'              => '/',
-            'rules'               => [
+            'suffix' => '/',
+            'rules' => [
                 '/' => 'site/index',
 
-                '<controller:\w+>/<id:\d+>'                                                  => '<controller>/view',
-                '<controller:[-_0-9a-zA-Z]+>/<action:[-_0-9a-zA-Z]+>/<id:\d+>'               => '<controller>/<action>',
-                '<controller:[-_0-9a-zA-Z]+>/<action:[-_0-9a-zA-Z]+>'                        => '<controller>/<action>',
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:[-_0-9a-zA-Z]+>/<action:[-_0-9a-zA-Z]+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:[-_0-9a-zA-Z]+>/<action:[-_0-9a-zA-Z]+>' => '<controller>/<action>',
                 '/<module:\w+>/<controller:[-_0-9a-zA-Z]+>/<action:[-_0-9a-zA-Z]+>/<id:\d+>' => '<module>/<controller>/<action>',
-                '/<module:\w+>/<controller:[-_0-9a-zA-Z]+>/<action:[-_0-9a-zA-Z]+>'          => '<module>/<controller>/<action>',
+                '/<module:\w+>/<controller:[-_0-9a-zA-Z]+>/<action:[-_0-9a-zA-Z]+>' => '<module>/<controller>/<action>',
             ]
         ],
 
         'i18n' => [
             'translations' => [
                 'app*' => [
-                    'class'            => 'yii\i18n\PhpMessageSource',
+                    'class' => 'yii\i18n\PhpMessageSource',
                     'forceTranslation' => true,
-                    'basePath'         => '@backend/messages',
-                    'fileMap'          => [
-                        'app/back'          => 'backend.php',
-                        'app/position'      => 'position.php',
-                        'app/color'         => 'color.php',
-                        'app/user'          => 'user.php',
-                        'app/animal'        => 'animal.php',
-                        'app/cowshed'       => 'cowshed.php',
-                        'app/farm'          => 'farm.php',
-                        'app/suspension'    => 'suspension.php',
-                        'app/diagnosis'     => 'diagnosis.php',
-                        'app/scheme'        => 'scheme.php',
-                        'app/preparation'   => 'preparation.php',
-                        'app/packing'       => 'packing.php',
+                    'basePath' => '@backend/messages',
+                    'fileMap' => [
+                        'app/back' => 'backend.php',
+                        'app/position' => 'position.php',
+                        'app/color' => 'color.php',
+                        'app/user' => 'user.php',
+                        'app/animal' => 'animal.php',
+                        'app/cowshed' => 'cowshed.php',
+                        'app/farm' => 'farm.php',
+                        'app/suspension' => 'suspension.php',
+                        'app/diagnosis' => 'diagnosis.php',
+                        'app/scheme' => 'scheme.php',
+                        'app/preparation' => 'preparation.php',
+                        'app/packing' => 'packing.php',
                         'app/groups-action' => 'groups-action.php',
-                        'app/action'        => 'action.php',
-                        'app/type-field'    => 'type-field.php',
-                        'app/type-list'     => 'type-list.php',
-                        'app/action-list'   => 'action-list.php',
+                        'app/action' => 'action.php',
+                        'app/type-field' => 'type-field.php',
+                        'app/type-list' => 'type-list.php',
+                        'app/action-list' => 'action-list.php',
                     ],
                 ]
             ]
         ]
     ],
-    'params'              => $params,
-    'modules'             => [
-        'gii'      => [
-            'class'      => 'yii\gii\Module',
+    'params' => $params,
+    'modules' => [
+        'gii' => [
+            'class' => 'yii\gii\Module',
             'allowedIPs' => ['*']
         ],
-        'scheme'   => [
+        'scheme' => [
             'class' => 'backend\modules\scheme\Module'
         ],
         'pharmacy' => [
--- backend/views/user/index.php
+++ backend/views/user/index.php
@@ -14,80 +14,92 @@ $this->params['breadcrumbs'][] = $this->title;
 /** @var $searchModel UserSearch */
 ?>
 
-    <div class="form-group">
-        <?php if (Yii::$app->user->can('userEdit')) : ?>
-            <?= Html::a(
-                Yii::t('app/user', 'USER_ADD'),
-                Url::toRoute(['user/new']),
-                ['class' => 'btn btn-primary']
-            ) ?>
-        <?php endif; ?>
-    </div>
 
-<?php echo GridView::widget([
-    "dataProvider" => $dataProvider,
-    "filterModel" => $searchModel,
-    'formatter' => [
-        'class' => 'yii\i18n\Formatter',
-        'nullDisplay' => '',
-    ],
-    'columns' => [
-        ['class' => 'yii\grid\SerialColumn'],
-        'lastName',
-        'firstName',
-        'middleName',
-        'username',
-        [
-            'attribute' => 'gender',
-            'value' => function (User $model) {
-                return Yii::t('app/user', 'USER_GENDER_' . $model->gender);
-            },
-            'filter' => Html::activeDropDownList(
-                $searchModel,
-                "gender",
-                User::getGenderList(),
-                [
-                    "prompt" => Yii::t('app/user', 'USER_GENDER'),
-                    'class' => 'form-control'
-                ]
-            )
-        ],
-        [
-            'attribute' => 'posName',
-            'value' => 'posName',
-            'filter' => Html::activeDropDownList(
-                $searchModel,
-                "posName",
-                Position::getAllPositions(),
-                [
-                    "prompt" => Yii::t('app/position', 'POSITION'),
-                    'class' => 'form-control'
-                ]
-            )
-        ],
-        [
-            'class' => 'yii\grid\ActionColumn',
-            'header' => Yii::t('app/user', 'ACTIONS'),
-            'template' => '<div class="btn-group">{update} {delete} </div>',
-            'visibleButtons' => [
-                'delete' => Yii::$app->user->can('userEdit'),
-            ],
-            'buttons' => [
-                'update' => function ($url, $model) {
-                    return Html::a(
-                        '<span class="fas fa-edit"></span>',
-                        Url::toRoute(['user/edit', 'id' => $model->id]),
-                        ['class' => 'btn btn-warning']
-                    );
-                },
-                'delete' => function ($url, $model) {
-                    return Html::a(
-                        '<span class="fas fa-trash"></span>',
-                        Url::toRoute(['user/delete', 'id' => $model->id]),
-                        ['class' => 'btn btn-danger']
-                    );
-                },
-            ],
-        ],
-    ]
-]);
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary">
+                <div class="card-body">
+                    <?php if (Yii::$app->user->can('userEdit')) : ?>
+                        <div class="form-group">
+                            <?= Html::a(
+                                Yii::t('app/user', 'USER_ADD'),
+                                Url::toRoute(['user/new']),
+                                ['class' => 'btn btn-primary']
+                            ) ?>
+                        </div>
+                    <?php endif; ?>
+
+                    <?php echo GridView::widget([
+                        "dataProvider" => $dataProvider,
+                        "filterModel" => $searchModel,
+                        'formatter' => [
+                            'class' => 'yii\i18n\Formatter',
+                            'nullDisplay' => '',
+                        ],
+                        'tableOptions' => ['class' => 'table table-sm table-hover table-bordered'],
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn'],
+                            'lastName',
+                            'firstName',
+                            'middleName',
+                            'username',
+                            [
+                                'attribute' => 'gender',
+                                'value' => function (User $model) {
+                                    return Yii::t('app/user', 'USER_GENDER_' . $model->gender);
+                                },
+                                'filter' => Html::activeDropDownList(
+                                    $searchModel,
+                                    "gender",
+                                    User::getGenderList(),
+                                    [
+                                        "prompt" => Yii::t('app/user', 'USER_GENDER'),
+                                        'class' => 'form-control'
+                                    ]
+                                )
+                            ],
+                            [
+                                'attribute' => 'posName',
+                                'value' => 'posName',
+                                'filter' => Html::activeDropDownList(
+                                    $searchModel,
+                                    "posName",
+                                    Position::getAllPositions(),
+                                    [
+                                        "prompt" => Yii::t('app/position', 'POSITION'),
+                                        'class' => 'form-control'
+                                    ]
+                                )
+                            ],
+                            [
+                                'class' => 'yii\grid\ActionColumn',
+                                'header' => Yii::t('app/user', 'ACTIONS'),
+                                'template' => '<div class="btn-group">{update} {delete} </div>',
+                                'visibleButtons' => [
+                                    'delete' => Yii::$app->user->can('userEdit'),
+                                ],
+                                'buttons' => [
+                                    'update' => function ($url, $model) {
+                                        return Html::a(
+                                            '<span class="fas fa-edit"></span>',
+                                            Url::toRoute(['user/edit', 'id' => $model->id]),
+                                            ['class' => 'btn btn-warning']
+                                        );
+                                    },
+                                    'delete' => function ($url, $model) {
+                                        return Html::a(
+                                            '<span class="fas fa-trash"></span>',
+                                            Url::toRoute(['user/delete', 'id' => $model->id]),
+                                            ['class' => 'btn btn-danger']
+                                        );
+                                    },
+                                ],
+                            ],
+                        ]
+                    ]); ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
