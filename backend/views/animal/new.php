5fc01b78ac0724ef947ab78a747d927f688da30b
a.radaev <a.radaev@mediasoft.team>
2020-09-17 13:05:24 +0400

    AdminLTE3. Добавление/редактирование животного

--- backend/views/animal/new.php
+++ backend/views/animal/new.php
@@ -9,69 +9,82 @@ use \yii\jui\DatePicker;
 /** @var Animal $model */
 
 $this->title = Yii::t('app/animal', 'ANIMAL_NEW');
+$this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<div class="box box-info">
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Заполните форму для создания</h3>
+                </div>
 
-    <?php $form = ActiveForm::begin(['action' => Url::toRoute(['animal/create']), 'id' => 'animal-form', 'class' => 'form-horizontal']); ?>
-    <div class="box-body">
+                <?php $form = ActiveForm::begin(['action' => Url::toRoute(['animal/create'])]); ?>
 
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'sex')->dropDownList(Animal::getListSexTypes(), [
-                    'class' => 'form-control'
-                ]) ?>
+                <div class="card-body">
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'nickname')->textInput([
+                                    'class' => 'form-control form-control-sm'
+                                ]) ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'label')->textInput([
+                                    'class' => 'form-control form-control-sm'
+                                ]) ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'physical_state')->dropDownList(Animal::getListPhysicalState(),
+                                    [
+                                        'class' => 'form-control form-control-sm'
+                                    ]
+                                ) ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'birthday')->widget(DatePicker::class, [
+                                    'language' => 'ru',
+                                    'dateFormat' => 'dd.MM.yyyy',
+                                    'options' => ['class' => 'form-control form-control-sm', 'autocomplete' => 'off']
+                                ]) ?>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'sex')->dropDownList(Animal::getListSexTypes(), [
+                                    'class' => 'form-control form-control-sm'
+                                ]) ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                        </div>
+                    </div>
+
+                </div>
+
+                <div class="card-footer">
+                    <?php if (Yii::$app->user->can('animalEdit')) : ?>
+                        <?= Html::submitButton(Yii::t('app/animal', 'ADD'), ['class' => 'btn btn-sm btn-primary']) ?>
+                    <?php endif; ?>
+                </div>
+
+                <?php ActiveForm::end(); ?>
             </div>
         </div>
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'physical_state')->dropDownList(Animal::getListPhysicalState(), [
-                    'class' => 'form-control'
-                ]) ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'nickname')->textInput([
-                    'class' => 'form-control'
-                ]) ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'label')->textInput([
-                    'class' => 'form-control'
-                ]) ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'birthday')->widget(DatePicker::class, [
-                    'language'   => 'ru',
-                    'dateFormat' => 'dd.MM.yyyy',
-                    'options'    => ['class' => 'form-control', 'autocomplete' => 'off']
-                ]) ?>
-            </div>
-        </div>
-
-        <!--<div class="form-group">
-            <div class="col-sm-12">
-                <? /*= $form->field($model, 'сowshed_id')->dropDownList([1=>1], [
-                    'class' => 'form-control'
-                ]) */ ?>
-            </div>
-        </div>-->
-
     </div>
-
-    <div class="box-footer">
-        <?= Html::submitButton(Yii::t('app/animal', 'ADD'), ['class' => 'btn btn-info pull-right', 'name' => 'contact-button']) ?>
-    </div>
-    <?php ActiveForm::end(); ?>
-
 </div>
