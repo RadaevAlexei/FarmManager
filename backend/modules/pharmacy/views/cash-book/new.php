93e1ade814001b55b9f395bea816911460e007ae
RadaevAlexei <alexeiradaev@gmail.com>
2020-09-18 23:33:29 +0400

    AdminLTE3. Формирование актов. Добавление расхода/прихода

--- backend/modules/pharmacy/views/cash-book/new.php
+++ backend/modules/pharmacy/views/cash-book/new.php
@@ -15,109 +15,122 @@ use backend\modules\pharmacy\models\Stock;
  */
 
 $this->title = 'Добавление';
+$this->params['breadcrumbs'][] = $this->title;
 
 ?>
 
-<div class="box box-info">
-
-    <?php $form = ActiveForm::begin([
-        'action' => Url::toRoute(['cash-book/create']),
-        'id'     => 'cash-book-form',
-        'class'  => 'form-horizontal'
-    ]); ?>
-    <div class="box-body">
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'user_id')->dropDownList(ArrayHelper::map(User::getAllList(),
-                    "id", "lastName"), [
-                    'id'     => 'select-user',
-                    'class'  => 'form-control',
-                    'prompt' => 'Кто проводит?'
-                ]) ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'type')->hiddenInput(['value' => $model->type])->label(false) ?>
-                <?= $form->field($model, 'type')->dropDownList(CashBook::getTypeList(), [
-                    'id'       => 'select-type',
-                    'class'    => 'form-control',
-                    'disabled' => true,
-                    'prompt'   => 'Выберите тип'
-                ]) ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'date')->widget(DatePicker::class, [
-                    'language'   => 'ru',
-                    'dateFormat' => 'dd.MM.yyyy',
-                    'options'    => ['class' => 'form-control', 'autocomplete' => 'off']
-                ]) ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'preparation_id')->dropDownList(ArrayHelper::map(Preparation::getAllList(),
-                    "id", "name"), [
-                    'id'     => 'select-preparation',
-                    'class'  => 'form-control',
-                    'prompt' => 'Выберите препарат?'
-                ]) ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'stock_id')->dropDownList(ArrayHelper::map(Stock::getAllList(),
-                    "id", "name"), [
-                    'id'     => 'select-stock',
-                    'class'  => 'form-control',
-                    'prompt' => 'Выберите склад?'
-                ]) ?>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <div class="col-sm-12">
-                <?= $form->field($model, 'count')->input('number', ['class' => 'form-control', 'min' => 0]) ?>
-            </div>
-        </div>
-
-        <?php if ($model->type == CashBook::TYPE_DEBIT) : ?>
-            <!--<div class="form-group">
-                <div class="col-sm-12">
-                    <? /*= $form->field($model, 'total_price')->input('number', [
-                        'class' => 'form-control',
-                        'min'   => 0,
-                        'step'  => 0.01,
-                    ]) */ ?>
+<div class="container-fluid">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Заполните форму для создания</h3>
                 </div>
-            </div>-->
 
-            <div class="form-group">
-                <div class="col-sm-12">
-                    <?= $form->field($model, 'vat_percent')->input('number', [
-                        'class' => 'form-control',
-                        'min'   => 0,
-                        'max'   => 100,
-                        'step'  => 0.01,
-                    ]) ?>
+                <?php $form = ActiveForm::begin(['action' => Url::toRoute(['cash-book/create'])]); ?>
+
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'type')->dropDownList(CashBook::getTypeList(), [
+                                    'id' => 'select-type',
+                                    'class' => 'form-control form-control-sm',
+                                    'disabled' => true,
+                                    'prompt' => 'Выберите тип'
+                                ]) ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'preparation_id')->dropDownList(ArrayHelper::map(Preparation::getAllList(),
+                                    "id", "name"), [
+                                    'id' => 'select-preparation',
+                                    'class' => 'form-control form-control-sm',
+                                    'prompt' => 'Выберите препарат?'
+                                ]) ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'user_id')->dropDownList(ArrayHelper::map(User::getAllList(),
+                                    "id", "lastName"), [
+                                    'id' => 'select-user',
+                                    'class' => 'form-control form-control-sm',
+                                    'prompt' => 'Кто проводит?'
+                                ]) ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'count')
+                                    ->input('number', ['class' => 'form-control form-control-sm', 'min' => 0]) ?>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'date')->widget(DatePicker::class, [
+                                    'language' => 'ru',
+                                    'dateFormat' => 'dd.MM.yyyy',
+                                    'options' => ['class' => 'form-control form-control-sm', 'autocomplete' => 'off']
+                                ]) ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <?php if ($model->type == CashBook::TYPE_DEBIT) : ?>
+                                <!--<div class="form-group">
+                                    <div class="col-sm-12">
+                                        <? /*= $form->field($model, 'total_price')->input('number', [
+                                            'class' => 'form-control form-control-sm',
+                                            'min'   => 0,
+                                            'step'  => 0.01,
+                                        ]) */ ?>
+                                    </div>
+                                </div>-->
+
+                                <div class="form-group">
+                                    <?= $form->field($model, 'vat_percent')->input('number', [
+                                        'class' => 'form-control form-control-sm',
+                                        'min' => 0,
+                                        'max' => 100,
+                                        'step' => 0.01,
+                                    ]) ?>
+                                </div>
+                            <?php endif; ?>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'stock_id')->dropDownList(ArrayHelper::map(Stock::getAllList(),
+                                    "id", "name"), [
+                                    'id' => 'select-stock',
+                                    'class' => 'form-control form-control-sm',
+                                    'prompt' => 'Выберите склад?'
+                                ]) ?>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <?= $form->field($model, 'type')
+                                ->hiddenInput(['value' => $model->type])
+                                ->label(false)
+                            ?>
+                        </div>
+                    </div>
                 </div>
+
+                <div class="card-footer">
+                    <?php if (Yii::$app->user->can('managePharmacyEdit')) : ?>
+                        <?= Html::submitButton('Добавить', ['class' => 'btn btn-sm btn-primary']) ?>
+                    <?php endif; ?>
+                </div>
+
+                <?php ActiveForm::end(); ?>
             </div>
-        <?php endif; ?>
-
+        </div>
     </div>
-
-    <div class="box-footer">
-        <?= Html::submitButton('Добавить',
-            ['class' => 'btn btn-info pull-right', 'name' => 'contact-button']) ?>
-    </div>
-
-    <?php ActiveForm::end(); ?>
-
 </div>
